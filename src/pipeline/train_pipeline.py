4a6ee5396d507fef2b8ef87d03327cf5e27f7ebf
Shrinath Rajput <rajputshrinath349@gmail.com>
2025-12-30 00:57:36 +0530

    Mtechproject

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -1,3 +1,4 @@
+import json
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
@@ -9,6 +10,12 @@ if __name__ == "__main__":
 
     transformation = DataTransformation()
     train_data, val_data = transformation.get_data_generators(dataset_path)
+
+    # 🔥 IMPORTANT: SAVE CLASS INDICES
+    with open("class_indices.json", "w") as f:
+        json.dump(train_data.class_indices, f, indent=4)
+
+    print("✅ CLASS INDICES SAVED:", train_data.class_indices)
 
     trainer = ModelTrainer()
     trainer.train(train_data, val_data)
---
+++ src/utils/preprocess.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+def preprocess_image(image_path, target_size=(224, 224)):
+    image = cv2.imread(image_path)
+
+    if image is None:
+        raise ValueError("Image not loaded")
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, target_size)
+    image = image.astype("float32") / 255.0
+    image = np.expand_dims(image, axis=0)
+
+    return image
+
